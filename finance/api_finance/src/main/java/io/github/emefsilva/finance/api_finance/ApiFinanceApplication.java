d30cc5207381f953b9162474a0db48315b1f061a
Emerson Ferreira <eme.ferreira.silva@gmail.com>
2024-11-28 00:07:29 -0300

    Initial structure project

---
+++ finance/api_finance/src/main/java/io/github/emefsilva/finance/api_finance/ApiFinanceApplication.java
@@ -0,0 +1,13 @@
+package io.github.emefsilva.finance.api_finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFinanceApplication.class, args);
+	}
+
+}
---
+++ finance/api_finance/src/test/java/io/github/emefsilva/finance/api_finance/ApiFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.emefsilva.finance.api_finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
